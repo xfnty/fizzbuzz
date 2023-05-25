89a1d66442966c34c74cb1ce3876cbcbbec13d66
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2023-05-25 21:18:13 +0700

    Added a few more exmaples

--- main.c
+++ main.c
@@ -14,9 +14,14 @@
     do {\
         clock_t start = clock();\
         {code}\
-        printf("Finished in %f seconds\n", (float)(clock() - start) / CLOCKS_PER_SEC);\
+        printf("----- Finished in %f seconds -----\n", (float)(clock() - start) / CLOCKS_PER_SEC);\
     } while(0);
 
+
+typedef struct fizzbuzz_s {
+    const char* name;
+    void (*func)(int n);
+} fizzbuzz_t;
 
 void typical_fizzbuzz(int n) {
     if (n % 3 == 0) {
@@ -95,21 +100,59 @@ void variable_msg_v2_fizzbuzz(int n) {
 }
 
 
+void single_printf_fizzbuzz(int n) {
+    const char* messages[] = {
+        "%d\n", "Fizz\n", "Buzz\n", "FizzBuzz\n"
+    };
+
+    printf(messages[((n % 3 == 0) | ((n % 5 == 0) << 1)) & 0b00000011], n);
+}
+
+
+// Source post: https://dev.to/shensd/taking-a-look-at-an-odd-fizzbuzz-solution-3hca
+// Author: wunkolo@github.com
+void literally_single_printf_fizzbuzz(int n) {
+    size_t i = n; // Could be in the argument list
+    printf("%zu\n\0\0\0\0Fizz\n\0\0\0Buzz\n\0\0\0FizzBuzz\n" 
+        + (((0x1241843 >> ((i % 15) * 2)) & 0b11) * 8), i);
+}
+
+
 int main(int argc, char const *argv[])
 {
-    if (argc <= 1) {
-        printf("usage: %s N\n", strrchr(argv[0], PATH_DIV) + 1);
+    const fizzbuzz_t fizzbuzzes[] = {
+        {"typical", typical_fizzbuzz},
+        {"single if (bitflag switch)", single_if_fizzbuzz},
+        {"variable message", variable_msg_fizzbuzz},
+        {"variable message v2", variable_msg_v2_fizzbuzz},
+        {"single printf", single_printf_fizzbuzz},
+        {"literally single printf", literally_single_printf_fizzbuzz},
+    };
+
+    if (argc < 3) {
+        printf("usage: %s N M\n", strrchr(argv[0], PATH_DIV) + 1);
+        printf("where N is the algorithm number and M is the highest number being reached.\n");
+        printf("Available algorithms are:\n");
+        for (int i = 0; i < sizeof(fizzbuzzes)/sizeof(fizzbuzzes[0]); i++)
+            printf("  %d: %s\n", i+1, fizzbuzzes[i].name);
         return 0;
     }
 
-    int m = atoi(argv[1]);
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+
+    if (n < 1 || n > sizeof(fizzbuzzes)/sizeof(fizzbuzzes[0])) {
+        printf("N is out of range.");
+        return -1;
+    }
+    if (m < 0) {
+        printf("M should not be negative.");
+        return -1;
+    }
 
     BENCHMARK(
         for (int i = 1; i < m + 1; i++) {
-            // typical_fizzbuzz(i);
-            // single_if_fizzbuzz(i);
-            // variable_msg_fizzbuzz(i);
-            variable_msg_v2_fizzbuzz(i);
+            fizzbuzzes[n-1].func(i);
         }
     );
 
